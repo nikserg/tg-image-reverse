e3b91da84a55b6a9402bf18c567d154c1dc3d576
Nikita Zarubin <n.zarubin@itcomgk.ru>
2022-03-16 12:43:27 +0300

    master

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/nikserg/tg-image-reverse
 
 go 1.17
+
+require (
+	github.com/cloudinary/cloudinary-go v1.6.0
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/nikserg/go-json-config v1.0.0
+)
+
+require (
+	github.com/creasty/defaults v1.5.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/nikserg/go-reverseimageapi v0.0.0-20220316083500-9b0c081bd09b
+)
--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	jsonconfig "github.com/nikserg/go-json-config"
+)
+
+type Config struct {
+	BotToken             string
+	ReverseImageApiToken string
+}
 
 func main() {
 	fmt.Println("Start")
+	var config Config
+	jsonconfig.ReadConfig("config.json", &config)
+
+	fmt.Printf("TG token %v \n", config.BotToken)
+	bot, err := tgbotapi.NewBotAPI(config.BotToken)
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	fmt.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			fmt.Printf("[%s] %v", update.Message.From.UserName, update.Message)
+			for _, photo := range update.Message.Photo {
+
+				picUrl, _ := bot.GetFileDirectURL(photo.FileID)
+				searchUrl := "https://yandex.ru/images/search?rpt=imageview&url=" + url.QueryEscape(picUrl)
+
+				fmt.Println(searchUrl)
+
+				response, err := http.Get(searchUrl)
+				if err != nil {
+					panic(err)
+				}
+				defer response.Body.Close()
+
+				bodyBytes, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					panic(err)
+				}
+				bodyString := string(bodyBytes)
+				var resultMessage string
+				if !strings.Contains(bodyString, "Таких же изображений не найдено") {
+					resultMessage = fmt.Sprintf("❌ фейк: %v", searchUrl)
+				} else {
+
+					resultMessage = fmt.Sprintf("✅ не фейк: %v", searchUrl)
+				}
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, resultMessage)
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+				break
+			}
+		}
+	}
 }
